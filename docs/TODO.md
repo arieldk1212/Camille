b49577e266a9e7e85f8c508a1351088396b0d973
arieldk1212 <arieldk6@gmail.com>
2025-12-23 15:33:02 +0200

    [base] structure, requirements

---
+++ docs/TODO.md
@@ -0,0 +1,9 @@
+# Camille TODO's
+
+## Request
+
+1. study and implement basic properties.
+
+## Response
+
+1. study on the basic important fields for the class.
---
+++ include/camille/request.h
@@ -0,0 +1,18 @@
+#ifndef CAMILLE_INCLUDE_CAMILLE_REQUEST_H_
+#define CAMILLE_INCLUDE_CAMILLE_REQUEST_H_
+
+namespace request {
+
+template <typename T>
+class Request {
+  public:
+
+  void getHeader();
+  void setHeader();
+  
+
+  private:
+};
+
+}  // namespace request
+#endif
