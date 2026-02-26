33b53898cfc639e032445afd454a89e717a714ba
arieldk1212 <arieldk6@gmail.com>
2026-02-26 17:41:09 +0200

    Confused
    Confused because the current architecture is bad, what should i do?

---
+++ include/camille/background.h
@@ -0,0 +1,10 @@
+#ifndef CAMILLE_INCLUDE_CAMILLE_BACKGROUND_H_
+#define CAMILLE_INCLUDE_CAMILLE_BACKGROUND_H_
+
+/**
+ * @brief The purpose of this file is to let the user decide when to run tasks on a backgroud thread
+ * to not block the main loop.
+ *
+ */
+
+#endif
---
+++ include/camille/casyncio.h
@@ -0,0 +1,9 @@
+#ifndef CAMILLE_INCLUDE_CAMILLE_CASYNCIO_H_
+#define CAMILLE_INCLUDE_CAMILLE_CASYNCIO_H_
+
+/**
+ * @brief this is file is made for debug purposes of the old io_context pool @ pool.h, need to
+ * benchmark it and test for optimizations.
+ */
+
+#endif
